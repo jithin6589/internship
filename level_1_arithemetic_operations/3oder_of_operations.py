19008c63ad192f2b23d52f139a7ac4859129ea68
jithin6589 <jithinbiju43.com>
2026-08-05 14:41:42 +0530

    the oder of operation program changed using function

---
+++ level_1_arithemetic_operations/3oder_of_operations.py
@@ -0,0 +1,15 @@
+# result=3+4*2-(6/3)
+# result2=3+4*2-(6//3)
+# print("the result is:",result)
+# print("the result2 is:",result2)
+
+
+def expression_result():
+    expression = 10 + 10 * 10 - (10 / 10)
+    return 
+
+def main():
+    expression2 = expression_result()
+    print(expression2)
+
+main()
