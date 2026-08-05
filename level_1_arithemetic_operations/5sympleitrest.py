1010b552b9b4134fff014d8030424b3c99f98d0c
jithin6589 <jithinbiju43.com>
2026-08-05 16:25:51 +0530

    symple intrest program updated with function

---
+++ level_1_arithemetic_operations/5sympleitrest.py
@@ -0,0 +1,5 @@
+P = float(input("Enter the principal amount: "))
+R = float(input("Enter the rate of interest: "))
+T = float(input("Enter the time (in years): "))
+result=(P*R*T)/100
+print("The simple interest is:", result)
