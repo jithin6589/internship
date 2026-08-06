9f20a93001a5c9d4564775ba15d68721d9ec935f
jithin6589 <jithinbiju43.com>
2026-08-06 14:06:47 +0530

    sum name edit on the program

--- level_2_logical_operations_conditionals/3nested_conditionals.py
+++ level_2_logical_operations_conditionals/3nested_conditionals.py
@@ -10,37 +10,24 @@
 #     if side2 == side3:
 #         print("Equilateral")
 
-def triangle_type(side1, side2, side3):
-
-    if side1 == side2:
-
-        if side2 == side3:
-            return "Equilateral"
-
+def triangle_finder(side1,side2,side3):
+    if side1==side2:
+        if side2==side3:
+            return("Equilateral")
         else:
-            return "Isosceles"
-
+            return("Isosceles")
     else:
-
-        if side2 == side3:
-            return "Isosceles"
-
+        if side2==side3:
+            return("Isosceles")
         else:
-
-            if side1 == side3:
-                return "Isosceles"
+            if side1==side3:
+                return("Isosceles")
             else:
-                return "Scalene"
-
-
+                return("Scalene")
 def main():
-
-    side1 = int(input("Enter first side: "))
-    side2 = int(input("Enter second side: "))
-    side3 = int(input("Enter third side: "))
-
-    result = triangle_type(side1, side2, side3)
-
-    print("Triangle Type:", result)
+    side1=int(input("Enter the side 1: "))
+    side2=int(input("Enter the side 2: "))
+    side3=int(input("Enter the side 3: "))
+    reslt=triangle_finder(side1,side2,side3)
+    print(f"The triangle is {reslt}")
 main()
-
